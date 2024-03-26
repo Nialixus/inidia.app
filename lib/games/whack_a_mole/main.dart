2c64a4fa6ac8ee36b7a0c6652ab0e0dd0b2171ad
Nialixus <nialixus@gmail.com>
2024-03-26 16:32:34 +0700

    add whack a mole module

---
+++ lib/games/whack_a_mole/main.dart
@@ -0,0 +1,5 @@
+library whack_a_mole;
+
+import 'package:flutter/material.dart';
+
+part 'views/home.dart';
---
+++ lib/games/whack_a_mole/views/home.dart
@@ -0,0 +1,10 @@
+part of '../main.dart';
+
+class WhackAMoleHomeScreen extends StatelessWidget {
+  const WhackAMoleHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
